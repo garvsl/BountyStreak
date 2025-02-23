8434713590fdc08ec3e742e2aa889c4eaa3f2112
garvsl <garvsehgal7@gmail.com>
2025-02-23 07:20:15 -0500

    login logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,26 @@
 import { ArrowRight } from "lucide-react-native";
 import { useRouter } from "expo-router";
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, TextInput } from "react-native";
 import { ModerateScale, VerticalScale } from "@/hooks/metrics";
+import { useState } from "react";
+import { createUserIfNotExists } from "firebaseConfig";
+import { useUser } from "@/hooks/useUser";
 
 export default function Home() {
   const router = useRouter();
+  const { setUser } = useUser();
+
+  const [username, setUsername] = useState("");
+
+  const handleLogin = async () => {
+    if (username.trim().length > 0) {
+      const res = await createUserIfNotExists(username);
+      if (res && res != null) {
+        router.replace("/(tabs)");
+        setUser(res);
+      }
+    }
+  };
 
   return (
     <View className="flex-1 justify-center items-center bg-[#070b0f] p-0">
@@ -21,12 +37,24 @@ export default function Home() {
           Get started with BountyStreak
         </Text> */}
 
+        <View className="flex flex-col gap-2">
+          <TextInput
+            className="bg-white p-3 text-lg border border-black rounded-2xl"
+            placeholder="email"
+            onChangeText={(e) => setUsername(e)}
+            value={username}
+          />
+          <TextInput
+            className="bg-white p-3 text-lg border border-black rounded-2xl"
+            placeholder="password"
+          />
+        </View>
         <Pressable
           style={{
             height: VerticalScale(55),
           }}
-          className="w-full mt-3 bg-white border border-black rounded-md flex-row items-center justify-center"
-          onPress={() => router.replace("/(tabs)")}
+          className="w-full mt-6 bg-white border border-black rounded-2xl flex-row items-center justify-center"
+          onPress={() => handleLogin()}
         >
           <Text
             style={{
